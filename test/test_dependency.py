0f36377b12fb889fcff47ca22f1070c57e60163c
alex-koziell <alexkoziell@gmail.com>
2021-03-12 14:11:12 +0000

    Update tests for spaCy v3

--- test/test_dependency.py
+++ test/test_dependency.py
@@ -1,8 +1,8 @@
 import pickle
 
 import pytest
-from spacy.tests.util import get_doc
 from spacy.vocab import Vocab
+from spacy.tokens import Doc
 
 from discopy.grammar.dependency import autonomous_tree, autonomous_tree_nocaps
 
@@ -13,9 +13,10 @@ def sentence():
     words = u"This is a test".split(" ")
     vocab = Vocab(strings=words)
     pos = ["DET", "VERB", "DET", "NOUN"]
-    heads = [1, 0, 1, -2]
+    deps = ["nsubj", "ROOT", "det", "attr"]
+    heads = [1, 1, 3, 1]
 
-    doc = get_doc(vocab=vocab, words=words, pos=pos, heads=heads)
+    doc = Doc(vocab, words=words, pos=pos, heads=heads, deps=deps)
     sentence = next(doc.sents)
 
     return sentence
